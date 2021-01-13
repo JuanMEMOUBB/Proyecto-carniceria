31b11595911c9672c34970218a11760b427ea345
JuanMEMOUBB <43252326+JuanMEMOUBB@users.noreply.github.com>
2021-01-13 12:48:07 -0300

    deploy

---
+++ precache-manifest.0165b9cc5ed140fb4db38375f14f3253.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "9198b92bd5ee74a86880",
+    "url": "/Proyecto-carniceria/css/about.1e1cd3de.css"
+  },
+  {
+    "revision": "9198b92bd5ee74a86880",
+    "url": "/Proyecto-carniceria/js/about.28853a12.js"
+  },
+  {
+    "revision": "fc1f56c61b8d1eaab3cf",
+    "url": "/Proyecto-carniceria/css/app.3e5c4b90.css"
+  },
+  {
+    "revision": "fc1f56c61b8d1eaab3cf",
+    "url": "/Proyecto-carniceria/js/app.17d8b6b6.js"
+  },
+  {
+    "revision": "c9c37c0459eac68e6afe",
+    "url": "/Proyecto-carniceria/js/chunk-2c7e6c6c.b75df32f.js"
+  },
+  {
+    "revision": "be22924b775cf579508a",
+    "url": "/Proyecto-carniceria/js/chunk-2d216214.49bf1fba.js"
+  },
+  {
+    "revision": "bdda8698921ac61af643",
+    "url": "/Proyecto-carniceria/js/chunk-2d219ff9.576870b4.js"
+  },
+  {
+    "revision": "8a90f6d8f0fee2b02941",
+    "url": "/Proyecto-carniceria/css/chunk-vendors.66e9bd5b.css"
+  },
+  {
+    "revision": "8a90f6d8f0fee2b02941",
+    "url": "/Proyecto-carniceria/js/chunk-vendors.5e002f08.js"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/Proyecto-carniceria/fonts/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/Proyecto-carniceria/fonts/nucleo-icons.ff208b8d.woff2"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/Proyecto-carniceria/fonts/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/Proyecto-carniceria/img/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "dcff44ba96eb275f827d81082085decc",
+    "url": "/Proyecto-carniceria/index.html"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/Proyecto-carniceria/favicon.png"
+  },
+  {
+    "revision": "fe6f1ca974844e9abe5f319fe390e706",
+    "url": "/Proyecto-carniceria/img/carne.jpg"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/Proyecto-carniceria/.gitkeep"
+  },
+  {
+    "revision": "99e58416b89637502b40ac8350eed85a",
+    "url": "/Proyecto-carniceria/img/faces/face-1.jpg"
+  },
+  {
+    "revision": "be74132f42ae1d3502f9a9b9fff68ac1",
+    "url": "/Proyecto-carniceria/Dashboard.PNG"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/Proyecto-carniceria/img/faces/face-3.jpg"
+  },
+  {
+    "revision": "eff0258abab1d65f1d560569502ca215",
+    "url": "/Proyecto-carniceria/img/carnes.jpg"
+  },
+  {
+    "revision": "231c3eb2acab3803302dcbb9331d8aa9",
+    "url": "/Proyecto-carniceria/img/cerdo.png"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/Proyecto-carniceria/img/faces/face-0.jpg"
+  },
+  {
+    "revision": "a9615bac158705203261e8348f574cc8",
+    "url": "/Proyecto-carniceria/img/default-avatar.png"
+  },
+  {
+    "revision": "0e953b1ea6d6e3addd7210e9c7c420a7",
+    "url": "/Proyecto-carniceria/img/faces/face-2.jpg"
+  },
+  {
+    "revision": "0b68eb8f1cde1fc9987a9196df05b96a",
+    "url": "/Proyecto-carniceria/img/faces/face-4.jpg"
+  },
+  {
+    "revision": "4ab22eca4053c14a34e4bdb6390deae9",
+    "url": "/Proyecto-carniceria/img/faces/face-5.jpg"
+  },
+  {
+    "revision": "bf1684a30a86ba1b222aab3acff16356",
+    "url": "/Proyecto-carniceria/img/faces/face-7.jpg"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/Proyecto-carniceria/img/favicon.png"
+  },
+  {
+    "revision": "4b87e628f4ef9988718860890b2a682f",
+    "url": "/Proyecto-carniceria/img/faces/face-6.jpg"
+  },
+  {
+    "revision": "43b98081492ac3bcb4a1fac6cf709403",
+    "url": "/Proyecto-carniceria/img/faces/tim_vector.jpe"
+  },
+  {
+    "revision": "0a337c37f03c0462996b7b03758fa72b",
+    "url": "/Proyecto-carniceria/img/loading-bubbles.svg"
+  },
+  {
+    "revision": "f575a04ebbb31b5798a4c54783e745a2",
+    "url": "/Proyecto-carniceria/img/new_logo.png"
+  },
+  {
+    "revision": "cd253e23ed052deeb80b42d2ed772183",
+    "url": "/Proyecto-carniceria/img/sidebar-3.jpg"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/Proyecto-carniceria/img/sidebar-1.jpg"
+  },
+  {
+    "revision": "44bf13a71a4db6e15913fe8af9296711",
+    "url": "/Proyecto-carniceria/img/tim_80x80.png"
+  },
+  {
+    "revision": "d27fbc90c2e644dfdc9765640dc713b9",
+    "url": "/Proyecto-carniceria/img/mask.png"
+  },
+  {
+    "revision": "9ebea76ee1225f00d882b21547a7b49f",
+    "url": "/Proyecto-carniceria/img/sidebar-5.jpg"
+  },
+  {
+    "revision": "7a4ce7cc040fc1cb8176cde106e9232f",
+    "url": "/Proyecto-carniceria/img/sidebar-2.jpg"
+  },
+  {
+    "revision": "c2a605fbc0e687b2e1b4b90a7c445cdd",
+    "url": "/Proyecto-carniceria/img/vue-logo.png"
+  },
+  {
+    "revision": "897b4cf909210560a84398d36da51983",
+    "url": "/Proyecto-carniceria/img/sidebar-4.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Proyecto-carniceria/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Proyecto-carniceria/precache-manifest.0165b9cc5ed140fb4db38375f14f3253.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "proyecto-carniceria"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
