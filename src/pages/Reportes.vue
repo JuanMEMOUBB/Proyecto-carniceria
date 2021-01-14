54f7f1b1c575227fb844f4b37d7b09c3d96d503c
Alex <31526974+Linksolidus@users.noreply.github.com>
2021-01-14 19:16:35 -0300

    tablas pdf listas

--- src/pages/Reportes.vue
+++ src/pages/Reportes.vue
@@ -1,9 +1,115 @@
 <template>
 <div>
+    <div class="row">
+        <div class= "col-md-6">
+            <b-card
+                title="Productos Vendidos"
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+            >
+                <b-card-text>
+                Cantidad de productos vendidos en el ultimo mes.
+                </b-card-text>    
+                <b-button @click="downloadVentaProductoXMes">Descargar</b-button>
+            </b-card>
+        </div>
 
-    <b-button @click="downloadVentaProductoXMes">Descargar</b-button>
-    <b-button @click="downloadIngresosXMes">Descargar</b-button>
+        <div class= "col-md-6">
+            <b-card
+                title="Ingresos"
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+            >
+                <b-card-text>
+                Cantidad de productos vendidos en el ultimo mes.
+                </b-card-text>    
+                <b-button @click="addEgresos">Descargar</b-button>
+            </b-card>
+        </div>
+    </div>
 
+
+    <b-modal  :id="infoModal.id" title="Egresos" ok-only @hide="downloadIngresosXMes">
+            <b-container>
+              <b-row class="my-1">
+                <b-col sm="2">
+                  <label for="input-number-1">Enero</label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input  id="input-number-1" v-model.number="infoModal.egresos[0]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-2">Febrero</label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-2" v-model.number="infoModal.egresos[1]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-3">Marzo</label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-3" v-model.number="infoModal.egresos[2]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-4">Abril</label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-4" v-model.number="infoModal.egresos[3]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-5">Mayo </label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-5" v-model.number="infoModal.egresos[4]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-6">Junio </label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-6" v-model.number="infoModal.egresos[5]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-7">Julio </label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-7" v-model.number="infoModal.egresos[6]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-8">Agosto </label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-8" v-model.number="infoModal.egresos[7]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-9">Septiembre </label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-9" v-model.number="infoModal.egresos[8]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-10">Octubre </label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-10" v-model.number="infoModal.egresos[9]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-11">Noviembre</label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-11" v-model.number="infoModal.egresos[10]"></b-form-input>
+                </b-col>
+                <b-col sm="2">
+                  <label for="input-default-12">Diciembre</label>
+                </b-col>
+                <b-col sm="10">
+                  <b-form-input id="input-default-12" v-model.number="infoModal.egresos[11]"></b-form-input>
+                </b-col>
+              </b-row>
+              <pre>{{ infoModal.content }}</pre>
+            </b-container>
+          </b-modal>
 
 </div>    
 </template>
@@ -52,6 +158,12 @@ export default {
             producto: null,
             totalProductoVendido: [],
 
+            infoModal:{
+                id: 'info-modal',
+                content: '',
+                egresos: [0,0,0,0,0,0,0,0,0,0,0,0],
+            }
+
         }
     },
     apollo:{
@@ -68,27 +180,31 @@ export default {
              const doc = new jsPDF()
            //doc.autoTable({ html: '#my-table' })
            let ingresos = this.reporteIngresosUltimosMeses();
-           console.log("hola")
-
+           for(let i = 0; i<12; i++){
+               if(ingresos[i] == null){
+                    ingresos[i]= 0;
+               }
+           }
+           
            let bodyPdf = [
-               ['Enero','$' + ingresos[0]],
-               ['Febrero','$' +ingresos[1]],
-               ['Marzo','$' +ingresos[2]],
-               ['Abril','$' +ingresos[3]],
-               ['Mayo','$' +ingresos[4]],
-               ['Junio','$' +ingresos[5]],
-               ['Julio','$' +ingresos[6]],
-               ['Agosto','$' +ingresos[7]],
-               ['Septiembre','$' +ingresos[8]],
-               ['Octubre','$' +ingresos[9]],
-               ['Noviembre','$' +ingresos[10]],
-               ['Diciembre','$' +ingresos[11]]
+               ['Enero','$' + ingresos[0], '$' + this.infoModal.egresos[0], '$' + (ingresos[0] - this.infoModal.egresos[0]) ],
+               ['Febrero',ingresos[1], '$' + this.infoModal.egresos[1], '$' + (ingresos[1] - this.infoModal.egresos[1]) ],
+               ['Marzo','$' +ingresos[2], '$' + this.infoModal.egresos[2], '$' + (ingresos[2] - this.infoModal.egresos[2]) ],
+               ['Abril','$' +ingresos[3], '$' +this.infoModal.egresos[3], '$' + (ingresos[3] - this.infoModal.egresos[3])],
+               ['Mayo','$' +ingresos[4], '$' + this.infoModal.egresos[4], '$' + (ingresos[4] - this.infoModal.egresos[4])],
+               ['Junio','$' +ingresos[5], '$' + this.infoModal.egresos[5], '$' + (ingresos[5] - this.infoModal.egresos[5])],
+               ['Julio','$' +ingresos[6], '$' + this.infoModal.egresos[6], '$' + (ingresos[6] - this.infoModal.egresos[6])],
+               ['Agosto','$' +ingresos[7], '$' + this.infoModal.egresos[7], '$' + (ingresos[7] - this.infoModal.egresos[7])],
+               ['Septiembre','$' +ingresos[8], '$' +this.infoModal.egresos[8], '$' + (ingresos[8] - this.infoModal.egresos[8])],
+               ['Octubre','$' +ingresos[9], '$' + this.infoModal.egresos[9], '$' + (ingresos[9] - this.infoModal.egresos[9])],
+               ['Noviembre','$' +ingresos[10], '$' + this.infoModal.egresos[10], '$' + (ingresos[10] - this.infoModal.egresos[10])],
+               ['Diciembre','$' +ingresos[11], '$' + this.infoModal.egresos[11], '$' + (ingresos[11] - this.infoModal.egresos[11])]
                ]
            
  
             // Or use javascript directly:
             doc.autoTable({
-            head: [['Mes', 'Ingreso']],
+            head: [['Mes', 'Ingreso','Egreso','Total']],
             body: bodyPdf,
             })
             
@@ -99,23 +215,6 @@ export default {
         
         downloadVentaProductoXMes(){
             let header= this.producto;
-          /*  for(let i = 0; i<this.producto.length;i++){
-                header[i] = this.producto[i].nombre;
-            }  */      
-/*
-            const pdf = new jsPDF();
-            //let header = ["id","name"];
-            let headerConfig = header.map(key=>({ 'nombre': key,
-            'prompt': key,
-            'width':50,
-            //'align':'center',
-            'padding':0}));
-            let data = [{id: 1, name: "Peter"},{id: 2, name: "Chris"}];
-            pdf.table(20, 30, data, headerConfig);
-            pdf.save("pdf.pdf");*/
-
-            //this.reporteVentaProductoXMes();
-           //console.log(this.totalProductoVendido);
            this.reporteVentaProductoXMes()
 
 
@@ -190,6 +289,16 @@ export default {
         },
 
 
+        //metodos modal
+        addEgresos(button){
+        this.$root.$emit('bv::show::modal', this.infoModal.id, button);
+      },
+      resetInfoModal() {
+      this.infoModal.content = '';
+      this.infoModal.egresos = [0,0,0,0,0,0,0,0,0,0,0,0];
+      },
+
+
 
     }
 
@@ -198,5 +307,6 @@ export default {
 </script>
 
 <style scoped>
+
 
 </style>
